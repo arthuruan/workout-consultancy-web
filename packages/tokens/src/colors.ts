5996401b79b5a7b007f34d426b1927ad556de787
Arthur Ruan <arthur123ruan@hotmail.com>
2023-07-25 22:58:09 -0300

    - Started design system

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  light: "#FFFFFF",
+
+  dark100: "#E9E9E9",
+  dark200: "#9AA0A6",
+  dark300: "#1E1E1E",
+  dark400: "#171717",
+  dark500: "#101010",
+
+  strengh300: "#F16D1A",
+};
